0d3bd2afc47d9e2effe4a7b45ef23aadba2f723e
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2019-12-11 10:54:33 +0100

    Program doesn't save each character as an image anymore, as it was just a test.

--- main.py
+++ main.py
@@ -1,149 +1,146 @@
 import os
 from PIL import Image
 
-os.mkdir('output-chars')
+def print_char_blocks():
+    chars_img = Image.open('chars.png')
+    chars_pix = chars_img.load()
 
-chars_img = Image.open('chars.png')
-chars_pix = chars_img.load()
+    width, height = chars_img.size  # Get the width and hight of the image for iterating over
 
-width, height = chars_img.size  # Get the width and hight of the image for iterating over
+    char_width = 18
+    char_height = 24
+    char_count = int((width) / char_width)
+    pixel_block_size = 3
 
-char_width = 18
-char_height = 24
-char_count = int((width) / char_width)
-pixel_block_size = 3
+    chars_blocks = []
+    char_blocks_sorted_dict = {}
 
-chars_blocks = []
-chars_blocks_sorted_dict = {}
+    chars = [
+        ' ',
+        '!',
+        '"',
+        '#',
+        '$',
+        '%',
+        '&',
+        "'",
+        '(',
+        ')',
+        '*',
+        '+',
+        ',',
+        '-',
+        '.',
+        '/',
+        '0',
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        ':',
+        ';',
+        '<',
+        '=',
+        '>',
+        '?',
+        '@',
+        'A',
+        'B',
+        'C',
+        'D',
+        'E',
+        'F',
+        'G',
+        'H',
+        'I',
+        'J',
+        'K',
+        'L',
+        'M',
+        'N',
+        'O',
+        'P',
+        'Q',
+        'R',
+        'S',
+        'T',
+        'U',
+        'V',
+        'W',
+        'X',
+        'Y',
+        'Z',
+        '[',
+        '\\',
+        ']',
+        '^',
+        '_',
+        '?',
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+        'f',
+        'g',
+        'h',
+        'i',
+        'j',
+        'k',
+        'l',
+        'm',
+        'n',
+        'o',
+        'p',
+        'q',
+        'r',
+        's',
+        't',
+        'u',
+        'v',
+        'w',
+        'x',
+        'y',
+        'z',
+        '{',
+        '|',
+        '}',
+        '~'
+    ]
 
-chars = [
-    ' ',
-    '!',
-    '"',
-    '#',
-    '$',
-    '%',
-    '&',
-    "'",
-    '(',
-    ')',
-    '*',
-    '+',
-    ',',
-    '-',
-    '.',
-    '/',
-    '0',
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    ':',
-    ';',
-    '<',
-    '=',
-    '>',
-    '?',
-    '@',
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'O',
-    'P',
-    'Q',
-    'R',
-    'S',
-    'T',
-    'U',
-    'V',
-    'W',
-    'X',
-    'Y',
-    'Z',
-    '[',
-    '\\',
-    ']',
-    '^',
-    '_',
-    '?',
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z',
-    '{',
-    '|',
-    '}',
-    '~'
-]
+    for char_index in range(char_count):
+        char_size = (18, 24)
+        char_img = Image.new('RGB', char_size, color = 'black')
+        char_pix = char_img.load()
+        char_colored_blocks = 0
 
-for char_index in range(char_count):
-    char_size = (18, 24)
-    char_img = Image.new('RGB', char_size, color = 'black')
-    char_pix = char_img.load()
-    char_colored_blocks = 0
+        for x in range(int(char_width / pixel_block_size)):
+            for y in range(int(char_height / pixel_block_size)):
+                pixel_block_rgb = chars_pix[char_index * char_width + x * pixel_block_size, y * pixel_block_size]
+                if pixel_block_rgb != (0, 0, 0):
+                    char_colored_blocks += 1
+                for bx in range (pixel_block_size):
+                    for by in range (pixel_block_size):
+                        char_pix[x * pixel_block_size + bx, y * pixel_block_size + by] = pixel_block_rgb
+        
+        char = chars[char_index]
+        chars_blocks.append([char, char_colored_blocks])
 
-    for x in range(int(char_width / pixel_block_size)):
-        for y in range(int(char_height / pixel_block_size)):
-            pixel_block_rgb = chars_pix[char_index * char_width + x * pixel_block_size, y * pixel_block_size]
-            if pixel_block_rgb != (0, 0, 0):
-                char_colored_blocks += 1
-            for bx in range (pixel_block_size):
-                for by in range (pixel_block_size):
-                    char_pix[x * pixel_block_size + bx, y * pixel_block_size + by] = pixel_block_rgb
-    
-    char = chars[char_index]
-    chars_blocks.append([char, char_colored_blocks])
+        if not char_colored_blocks in char_blocks_sorted_dict:
+            char_blocks_sorted_dict[char_colored_blocks] = []
+        char_blocks_sorted_dict[char_colored_blocks].append(char)
 
-    if not char_colored_blocks in chars_blocks_sorted_dict:
-        chars_blocks_sorted_dict[char_colored_blocks] = []
-    chars_blocks_sorted_dict[char_colored_blocks].append(char)
+    char_blocks_sorted = chars_blocks.copy()
+    char_blocks_sorted.sort(key = lambda x: x[1])
 
-    char_img.save('output-chars/' + ' ' + str(char_index) + '-' + str(char_colored_blocks) + '.png')
+    print("\nchar_blocks_sorted: ")
+    print(char_blocks_sorted)
+    print("\nchar_blocks_sorted_dict: ")
+    print(char_blocks_sorted_dict)
 
-chars_blocks_sorted = chars_blocks.copy()
-chars_blocks_sorted.sort(key = lambda x: x[1])
-
-# print("\nchars_blocks: ")
-# print(chars_blocks)
-# print("\nchars_blocks_sorted: ")
-# print(chars_blocks_sorted)
-print("\nchars_blocks_sorted_dict: ")
-print(chars_blocks_sorted_dict)
+# print_char_blocks()
