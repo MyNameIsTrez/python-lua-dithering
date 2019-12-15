2e363ee8d297180a7be9fb46ef22a3cad955d093
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2019-12-15 03:58:47 +0100

    Made a new algorithm that saves a string of each frame, every time a frame has been processed into chars.

--- dithering.py
+++ dithering.py
@@ -1,3 +1,7 @@
+## IMPORTING ####################
+
+from bisect import bisect_left
+
 ## README ####################
 
 # This file contains a public table, which contains every character in Tekkit Classic's 1.33 ComputerCraft character set.
@@ -8,70 +12,41 @@
 
 ## UNEDITABLE VARIABLES ####################
 
-brightnessChars = {
-	0: [' '],
-	2: ['.'],
-	3: ["'", ','],
-	4: [':'],
-	5: ['-', ';', '^', '_'],
-	6: ['!', '"', '*', 'i', '|', '~'],
-	7: ['/', '<', '>', '\\', 'l'],
-	9: ['(', ')', '+', '?', 'Y', '?', 'r', 'v', 'x', '{', '}'], # 't' used to be here, between 'r' and 'v'. It is a reserved character to indicate not having to draw.
-	10: ['=', 'J'],
-	11: ['%', 'I', 'L', 'T', '[', ']', 'c', 'f', 'j'],
-	12: ['1', '7', 'k', 'n', 'o', 'u'],
-	13: ['C', 'F', 'V', 'X', 'm', 's', 'z'],
-	14: ['3', 'P', 'a', 'h', 'p', 'q', 'w'],
-	15: ['$', '&', '4', '6', '9', 'K', 'S', 'U', 'Z', 'e', 'y'],
-	16: ['2', 'O', 'Q', 'b', 'd'],
-	17: ['5', '8', 'E', 'G', 'H', 'M', 'N', 'W', 'g'],
-	18: ['A', 'D', 'R'],
-	19: ['0'],
-	20: ['#', 'B'],
-	24: ['@']
-}
+chars = [
+	' ', '.', "'", ':', '-', '!', '/', '(', '=', '%',
+	'1', 'C', '3', '$', '2', '5', 'A', '0', '#', '@'
+]
+
+charIndices = [
+	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+	11, 12, 13, 14, 15, 16, 17, 18, 19
+]
 
 ## FUNCTIONS ####################
 
-def getHighestIndex():
-	highestIndex = 1
-	for index in list(brightnessChars):
-		if index > highestIndex:
-			highestIndex = index
-	return highestIndex
+def take_closest(myNumber):
+	"""
+	Assumes myList is sorted. Returns closest value to myNumber.
 
-highestIndex = getHighestIndex()
-
-def getClosestIndex(floatIndex):
-	keys = list(brightnessChars)
-	firstIndex = keys[0]
-	
-	for index in list(brightnessChars):
-		if index < floatIndex:
-			previousIndex = index
-		else:
-			# previousIndex doesn't exist when floatIndex is 0, so return the first index.
-			if index == firstIndex:
-				return index
-			
-			# The closest index is previousIndex or index.
-			# It's the one which is tne closest to floatIndex.
-			previousIndexDiff = abs(floatIndex - previousIndex)
-			indexDiff = abs(floatIndex - index)
-
-			# If they are equal to each other, return index.
-			# I treated this as an arbitrary decision, but maybe it isn't an arbitrary choice to make.
-			if previousIndexDiff < indexDiff:
-				return previousIndex
-			else:
-				return index
+	If two numbers are equally close, return the smallest number.
+	"""
+	pos = bisect_left(charIndices, myNumber)
+	if pos == 0:
+		return charIndices[0]
+	if pos == len(charIndices):
+		return charIndices[-1]
+	before = charIndices[pos - 1]
+	after = charIndices[pos]
+	if after - myNumber < myNumber - before:
+	   return after
+	else:
+	   return before
 
 # The given n should be between 0 and 1, both inclusive.
 def getClosestChar(n):
 	if n < 0 or n > 1:
 		raise invalidInput("getClosestChar expected a float between 0 and 1, both inclusive")
 	
-	floatIndex = n * highestIndex
-	closestIndex = getClosestIndex(floatIndex)
-	closestCharTable = brightnessChars[closestIndex]
-	return closestCharTable[0] # For now, we always give the first character of the index its table.
+	floatIndex = n * 20 # there are 20 chars that can be picked
+	closestIndex = take_closest(floatIndex)
+	return chars[closestIndex]
--- test.py
+++ test.py
@@ -1,12 +1,27 @@
 import time
 import random
-import string
+from random import random
+import dithering
 
+last_string = None
 t0 = time.time()
-b = []
-for i in range(10**7):
-    a = random.choice(string.ascii_lowercase)
-    b.append(a)
-open("bla.txt", "w").write(''.join(b))
+# open('random.txt', 'w')
+with open('random.txt', 'a') as file:
+    for f in range(5301):
+        t1 = time.time()
+        string = ''
+        for x in range(426):
+            for y in range(160):
+                char = dithering.getClosestChar(random())
+                if last_string == None or char != last_string[y + x * 160]:
+                    string = string+char
+                else:
+                    string = string+'t'
+        last_string = string
+        file.write(string)
+        progress = 'frame '+str(f)+'/5301'
+        speed = str(round(time.time()-t1, 2)) + 's/frame'
+        print(progress+', '+speed, end='\r', flush=True)
+
 d = time.time() - t0
-print ("duration: %.2f s." % d)
+print ('\nduration: %.2fs.' % d)
