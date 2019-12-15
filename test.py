f20b4dd4e9635baa404c699ad608fe2fad5fc3fb
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2019-12-15 15:28:51 +0100

    Now writing width, height and frame_count to a file.

--- test.py
+++ test.py
@@ -1,63 +1,68 @@
+import os
 import time
-import random
 import cv2
 from PIL import Image
-from random import random # temporary
-from memory_profiler import profile
 import dithering
 
-@profile
 def process_frames():
-	i = 0
-	while True:
-		t1 = time.time()
-		hasFrames, cv2_frame = video.read()
-		if hasFrames:
-			cv2_frame = cv2.cvtColor(cv2_frame, cv2.COLOR_BGR2RGB) # is this necessary?
-			pil_frame = Image.fromarray(cv2_frame)
-			pil_frame = pil_frame.convert('RGBA')
-			
-			# resize the image to the desired width and height
-			pil_frame = pil_frame.resize((new_width, new_height), Image.ANTIALIAS)
-			# process_frame(pil_frame, i)
-			
-			progress = 'frame '+str(i)+'/5301'
-			speed = str(round(time.time()-t1, 2)) + 's/frame'
-			print(progress+', '+speed, end='\r', flush=True)
-			
-			i = i + 1
-		else:
-			video.release()
-			break
+	if not os.path.isfile('random.txt'):
+		# prepare file for data
+		string = '{width=' + '{},height={},frame_count={},optimized_frames=\''.format(
+			new_width, new_height, frame_count)
+		open('random.txt', 'w').write(string)
+
+		# i = 0
+		# while True:
+		# 	hasFrames, cv2_frame = video.read()
+		# 	if hasFrames:
+		# 		cv2_frame = cv2.cvtColor(cv2_frame, cv2.COLOR_BGR2RGB) # is this necessary?
+		# 		pil_frame = Image.fromarray(cv2_frame)
+		# 		pil_frame = pil_frame.convert('RGBA')
+				
+		# 		# resize the image to the desired width and height
+		# 		pil_frame = pil_frame.resize((new_width, new_height), Image.ANTIALIAS)
+				
+		# 		process_frame(pil_frame, i)
+				
+		# 		i = i + 1
+		# 	else:
+		# 		video.release()
+		# 		break
+		
+		string = '\',initial_frame=\''
+		open('random.txt', 'a').write(string)
+		
+		string = '\'}'
+		open('random.txt', 'a').write(string)
 
 last_string = None
 def process_frame(frame, i):
 	global last_string
+	frame_pixels = frame.load()
 	t1 = time.time()
 	string = ''
-	# frame_pixels = frame.load()
 	for x in range(new_width):
 		for y in range(new_height):
-			# brightness = get_brightness(frame_pixels[x, y])
-			# char = dithering.getClosestChar(brightness)
-			char = dithering.getClosestChar(random())
-			# if last_string == None or char != last_string[y + x * 160]:
-			string = string+char
-			# else:
-			# 	string = string+'t'
+			brightness = get_brightness(frame_pixels[x, y])
+			char = dithering.getClosestChar(brightness)
+			if last_string == None or char != last_string[y + x * 160]:
+				string = string+char
+			else:
+				string = string+'t'
 	last_string = string
-	# open('random.txt', 'a').write(string)
+	open('random.txt', 'a').write(string)
 	progress = 'frame '+str(i)+'/5301'
+	i = i + 1
 	speed = str(round(time.time()-t1, 2)) + 's/frame'
 	print(progress+', '+speed, end='\r', flush=True)
 
-# def get_brightness(tup):
-# 	# red green and blue aren't equally bright
-# 	brightness = (0.2126 * tup[0] + 0.7152 * tup[1] + 0.0722 * tup[2]) / 255
-# 	if len(tup) == 4:
-# 		return brightness * tup[3] / 255
-# 	else:
-# 		return 0
+def get_brightness(tup):
+	# red, green and blue aren't equally bright
+	brightness = (0.2126 * tup[0] + 0.7152 * tup[1] + 0.0722 * tup[2]) / 255
+	if len(tup) == 4:
+		return brightness * tup[3] / 255
+	else:
+		return 0
 
 new_width_stretched = True
 max_width = 426
@@ -72,7 +77,9 @@ new_height = max_height
 if new_width_stretched:
 	new_width = max_width - 1 # the CC config's terminal_width variable has to be subtracted by 1
 else:
-	new_width = int(new_height * old_width / old_height)	
+	new_width = int(new_height * old_width / old_height)
+
+frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT)) # inaccurate, but fast
 
 t0 = time.time()
 process_frames()
