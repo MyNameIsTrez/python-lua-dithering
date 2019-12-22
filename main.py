5912add1636689f5f7ce2ac8b4f2817d46a7c6a3
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2019-12-22 01:55:06 +0100

    Optimized and added used_frame_count.

--- main.py
+++ main.py
@@ -42,45 +42,51 @@ def process_frames(full_file_name, computer_type):
 		output_file.write(string)
 
 		i = 0
-		
+		used_frame_count = 0
 		if extension == 'mp4':
+			frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT)) # inaccurate, but fast
+			
 			while True:
 				hasFrames, cv2_frame = video.read()
-				frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT)) # inaccurate, but fast
 				if hasFrames:
-					cv2_frame = cv2.cvtColor(cv2_frame, cv2.COLOR_BGR2RGB) # is this necessary?
-					pil_frame = Image.fromarray(cv2_frame)
-					pil_frame = pil_frame.convert('RGBA')
-					
-					# resize the image to the desired width and height
-					pil_frame = pil_frame.resize((new_width, new_height), Image.ANTIALIAS)
-					
-					process_frame(pil_frame, i, new_width, new_height, output_file, frame_count)
-					
-					i = i + 1
+					if i % frame_skipping == 0:
+						cv2_frame = cv2.cvtColor(cv2_frame, cv2.COLOR_BGR2RGB) # is this necessary?
+						pil_frame = Image.fromarray(cv2_frame)
+						pil_frame = pil_frame.convert('RGBA')
+						
+						# resize the image to the desired width and height
+						pil_frame = pil_frame.resize((new_width, new_height), Image.ANTIALIAS)
+						process_frame(pil_frame, i, new_width, new_height, output_file, frame_count)
+						
+						used_frame_count += 1
+					i += 1
 				else:
 					video.release()
 					break
 		elif extension == 'gif':
 			# mypalette = old_image.getpalette() # possibly helps
 			try:
-				while 1:
-					# old_image.putpalette(mypalette) # possibly helps
-					new_image = old_image.resize((new_width, new_height), Image.ANTIALIAS)
-					process_frame(new_image, i, new_width, new_height, output_file, None)
-					old_image.seek(old_image.tell() + 1) # gets the next frame
-					i = i + 1
+				while True:
+					if i % frame_skipping == 0:
+						# old_image.putpalette(mypalette) # possibly helps
+						new_image = old_image.resize((new_width, new_height), Image.ANTIALIAS)
+						process_frame(new_image, i, new_width, new_height, output_file, None)
+						old_image.seek(old_image.tell() + 1) # gets the next frame
+						
+						used_frame_count += 1
+					i += 1
 			except:
 				frame_count = i # continue
 		elif extension == 'jpeg':
 			new_image = old_image.resize((new_width, new_height), Image.ANTIALIAS)
 			new_image = new_image.convert('RGB')
-			process_frame(new_image, i, new_width, new_height, output_file, 1)
-			frame_count = 1
+			process_frame(new_image, 0, new_width, new_height, output_file, 1)
+			
+			used_frame_count = 1
 		else:
 			print('Entered an invalid file type; only mp4, gif and jpeg are allowed!')
 		
-		string = '}},frame_count={}}}'.format(frame_count) # '}}' necessary, because you get a 'ValueError' with '}'
+		string = '}},frame_count={}}}'.format(used_frame_count) # '}}' necessary, because you get a 'ValueError' with '}'
 		output_file.write(string)
 		print()
 
@@ -88,10 +94,6 @@ def process_frames(full_file_name, computer_type):
 
 def process_frame(frame, i, new_width, new_height, output_file, frame_count):
 	t1 = time.time()
-	
-	# return to skip this frame
-	if i % frame_skipping != 0:
-		return
 	
 	frame = frame.convert('RGBA')
 	frame_pixels = frame.load()
@@ -134,7 +136,6 @@ def process_frame(frame, i, new_width, new_height, output_file, frame_count):
 				
 				prev_char_count = 1
 				prev_char = char
-					
 
 		# string=string+"|"
 	
@@ -145,7 +146,7 @@ def process_frame(frame, i, new_width, new_height, output_file, frame_count):
 		progress = progress+str(frame_count)
 	else:
 		progress = progress+'?'
-	i = i + 1
+	i = i + 1 # why is this here?
 	elapsed = time.time()-t1
 	speed = '{:.2f}s/frame'.format(elapsed)
 	if frame_count:
