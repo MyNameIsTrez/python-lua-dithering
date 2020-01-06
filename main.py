3c1cf7f4246c40b06e27199be65aa3c2bdc06cf3
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2020-01-06 04:17:33 +0100

    Now measuring the time an entire frame takes to be processed, and the time it takes for each pixel in the frame to be looped over.

--- main.py
+++ main.py
@@ -44,6 +44,8 @@ def process_frames(full_file_name, computer_type):
 			frame_count = floor(actual_frame_count / frame_skipping)
 
 			while True:
+				start_frame_time = time.time()
+
 				hasFrames, cv2_frame = video.read()
 				if hasFrames:
 					if i % frame_skipping == 0:
@@ -56,8 +58,9 @@ def process_frames(full_file_name, computer_type):
 
 						# resize the image to the desired width and height
 						pil_frame = pil_frame.resize((new_width, new_height), Image.ANTIALIAS)
-						process_frame(pil_frame, used_frame_count, new_width, new_height,
-						              output_file, frame_count)
+
+						process_frame(pil_frame, used_frame_count, new_width,
+						              new_height, output_file, frame_count, start_frame_time)
 					i += 1
 				else:
 					video.release()
@@ -66,6 +69,8 @@ def process_frames(full_file_name, computer_type):
 			# mypalette = old_image.getpalette() # possibly helps
 			try:
 				while True:
+					start_frame_time = time.time()
+
 					if i % frame_skipping == 0:
 						# old_image.putpalette(mypalette) # possibly helps
 						new_image = old_image.resize((new_width, new_height), Image.ANTIALIAS)
@@ -73,19 +78,21 @@ def process_frames(full_file_name, computer_type):
 						used_frame_count += 1
 
 						process_frame(new_image, used_frame_count, new_width,
-						              new_height, output_file, None)
+						              new_height, output_file, None, start_frame_time)
 						old_image.seek(old_image.tell() + 1)  # gets the next frame
 					i += 1
 			except:
 				frame_count = i  # continue
 		elif extension == 'jpeg' or extension == 'png' or extension == 'jpg':
+			start_frame_time = time.time()
+
 			new_image = old_image.resize((new_width, new_height), Image.ANTIALIAS)
 			# new_image = new_image.convert('RGB')
 
 			used_frame_count += 1
 
 			process_frame(new_image, used_frame_count,
-			              new_width, new_height, output_file, 1)
+			              new_width, new_height, output_file, 1, start_frame_time)
 		else:
 			print('Entered an invalid file type; only mp4, gif, jpeg, png and jpg extensions are allowed!')
 
@@ -102,9 +109,7 @@ def process_frames(full_file_name, computer_type):
 		os.rename(output_path, renamed_output_path)
 
 
-def process_frame(frame, used_frame_count, new_width, new_height, output_file, frame_count):
-	t1 = time.time()
-
+def process_frame(frame, used_frame_count, new_width, new_height, output_file, frame_count, start_frame_time):
 	frame = frame.convert('RGBA')
 	frame_pixels = frame.load()
 
@@ -113,6 +118,8 @@ def process_frame(frame, used_frame_count, new_width, new_height, output_file, f
 	string = ''
 
 	modified_width = new_width - 1
+
+	looping_start_time = time.time()
 
 	for y in range(new_height):
 		for x in range(modified_width):
@@ -154,11 +161,14 @@ def process_frame(frame, used_frame_count, new_width, new_height, output_file, f
 		if y < new_height - 1:
 			string += '\\n'
 
-	# gives each frame its own line, so it can be easily found and parsed
+	looping_end_time = time.time()
+
+	# gives each frame its own line in the outputted file, so lines can easily be found and parsed
 	if used_frame_count > 1:
 		final_string = '\n' + string
 	else:
 		final_string = string
+
 	output_file.write(final_string)
 
 	# progress
@@ -166,34 +176,53 @@ def process_frame(frame, used_frame_count, new_width, new_height, output_file, f
 	if frame_count:
 		progress = progress + str(frame_count)
 	else:
-		progress = progress + '1000+'
+		progress = progress + '?'
 
 	# speed
-	elapsed = time.time() - t1
+	elapsed = time.time() - start_frame_time
 	if elapsed > 0:
 		processed_fps = floor(1 / elapsed)
 	else:
 		processed_fps = '1000+'
 	speed = '{} frames/s'.format(processed_fps)
 
+	# speed 2
+	elapsed_2 = looping_end_time - looping_start_time
+	if elapsed_2 > 0:
+		processed_fps = floor(1 / elapsed_2)
+	else:
+		processed_fps = '1000+'
+	speed_2 = '{} frames/s'.format(processed_fps)
+
 	# eta
 	if frame_count:
 		frames_left = frame_count - used_frame_count
 		seconds_left = elapsed * frames_left
-		eta_minutes = floor(seconds_left / 60)
+
+		eta_hours = floor(seconds_left / 3600)
+		eta_minutes = floor(seconds_left / 60) % 60
 		eta_seconds = floor(seconds_left) % 60
-		eta = '{}m, {}s left'.format(eta_minutes, eta_seconds)
+
+		# Makes sure each value is always 2 characters wide when printed.
+		if eta_hours < 10:
+			eta_hours = '0' + str(eta_hours)
+		if eta_minutes < 10:
+			eta_minutes = '0' + str(eta_minutes)
+		if eta_seconds < 10:
+			eta_seconds = '0' + str(eta_seconds)
+
+		eta = '{}:{}:{} left'.format(eta_hours, eta_minutes, eta_seconds)
 	else:
 		eta = '? left'
 
 	clear = '		'
 
-	print('    ' + progress + ', ' + speed +
+	print('    ' + progress + ', total speed: ' + speed + ', pixel loop speed: ' + speed_2 +
 	      ', ' + eta + clear, end='\r', flush=True)
 
 
 def get_brightness(tup):
-	# red, green and blue aren't equally bright
+	# red, green and blue values aren't equally bright to the human eye
 	brightness = (0.2126 * tup[0] + 0.7152 * tup[1] + 0.0722 * tup[2]) / 255
 	if len(tup) == 4:
 		return brightness * tup[3] / 255
@@ -208,11 +237,11 @@ computer_type = 'desktop'
 compressed_output = False
 new_width_stretched = True
 
-# 1 means every frame is kept, 3 means every third frame is kept.
-# this is a file compression method
+# a file compression method
+# 1 means every frame is kept, 3 means every third frame is kept
 frame_skipping = 1
 
-# see tekkit/config/mod_ComputerCraft.cfg
+# see tekkit/config/mod_ComputerCraft.cfg to set your own max_width and max_height values
 if computer_type == 'laptop':
 	max_width = 227
 	max_height = 85
@@ -232,9 +261,9 @@ for name in names:
 	# moving file
 	os.rename('inputs/' + name, 'temp inputs/' + name)
 
-d = time.time() - t0
+time_elapsed = time.time() - t0
 
-minutes = floor(d / 60)
-seconds = d % 60
+minutes = floor(time_elapsed / 60)
+seconds = time_elapsed % 60
 
 print('Done! Duration: {}m, {:.2f}s'.format(minutes, seconds))
